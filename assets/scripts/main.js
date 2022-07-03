30f2186769d8ea50327841aae5ebe66b914f4f4e
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2022-07-04 00:22:45 +0100

    FIM
    FI

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,29 +8,40 @@ function validateform() {
       
   if (name == null || name == "") {  
     document.getElementsByName('name')[0].placeholder = 'Please fill name'; 
-    updateColorInvalid();
+    updateColorInvalidName();
     return false;  
-  }else if(email.length < 4) {  
+  }
+  if(email.length < 4) {  
     document.getElementsByName('email')[0].placeholder = 'Invalid email'; 
-    updateColorInvalid(); 
+    updateColorInvalidEmail(); 
     return false;  
-  }else if(message.length < 10) {  
+  }
+  if(message.length < 10) {  
     document.getElementsByName('message')[0].placeholder = 'Message must be at least 10 characters long'; 
-    updateColorInvalid();  
+    updateColorInvalidMessage();  
     return false;  
   }else {
-    document.getElementsByName('message')[0].placeholder = 'Submitted Successfully!';
-    updateColorSuccess();
+    //document.getElementsByName('message')[0].placeholder = 'Submitted Successfully!';
+    //updateColorSuccess();
+    alert("Submitted Successfully!");
     return true;
   }
 }
 
-function updateColorInvalid() {
-  document.querySelector('input[type=text]').style.setProperty("--c", "#ff7b7b");
+function updateColorInvalidName() {
+  document.querySelector('input[name=name]').style.setProperty("--c", "#ff7b7b");
+}
+
+function updateColorInvalidEmail() {
+  document.querySelector('input[name=email]').style.setProperty("--c", "#ff7b7b");
+}
+
+function updateColorInvalidMessage() {
+  document.querySelector('input[name=message]').style.setProperty("--c", "#ff7b7b");
 }
 
 function updateColorSuccess() {
-  document.querySelector('input[type=text]').style.setProperty("--c", "#B7EFA1");
+  document.querySelector('input[name=message]').style.setProperty("--c", "#B7EFA1");
 }
 
 /*-------------------------------------
@@ -40,4 +51,3 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-    
